57ef78fdeabf7f394c523054b037d6328fe0ba5e
perryx05 <minhchien0305@gmail.com>
2025-06-20 06:03:08 +0000

    [automatic commit] 2025-06-20 06:03:08 UTC

--- part1/app.js
+++ part1/app.js
@@ -25,3 +25,15 @@ app.get('/api/dogs', async (req, res) => {
     res.status(500).json({error: 'Failed to fetch dogs', details: err.message});
  }
 });
+
+app.get('/api/dogs', async (req, res) => {
+  try{
+   const [rows] = await db.execute(`
+     Select d.name AS dog_name, d.size, u.username AS owner_username
+     FROM Dogs d
+     JOIN Users u ON d.owner_id = u.user_id`);
+     res.json(rows);
+  } catch (err){
+     res.status(500).json({error: 'Failed to fetch dogs', details: err.message});
+  }
+ });
