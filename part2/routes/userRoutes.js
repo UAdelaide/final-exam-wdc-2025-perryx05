ce7fb2035dae4d3da770c06655d1eea37e0bab5a
perryx05 <minhchien0305@gmail.com>
2025-06-20 09:09:58 +0000

    [automatic commit] 2025-06-20 09:09:58 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -53,7 +53,7 @@ router.post('/login', async (req, res) => {
       user_id: rows[0].user_id,
       username: rows[0].username,
       role: rows[0].role
-    }
+    };
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
