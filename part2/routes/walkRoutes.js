301b3cc0c342a058844197aef10ab3d9f8db0013
perryx05 <minhchien0305@gmail.com>
2025-06-20 13:52:11 +0000

    [automatic commit] 2025-06-20 13:52:11 UTC

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -73,7 +73,7 @@ router.get('/my-dogs', async (req, res) => {
   }
 });
 
-router.get('/api/dogs', async (req, res) => {
+router.get('/dogs', async (req, res) => {
   try{
    const [rows] = await db.execute(`
      Select d.name AS dog_name, d.size, u.username AS owner_username
