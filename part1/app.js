674a4336687367c07a37a160d08db9fed515acda
perryx05 <minhchien0305@gmail.com>
2025-06-20 06:04:50 +0000

    [automatic commit] 2025-06-20 06:04:50 UTC

--- part1/app.js
+++ part1/app.js
@@ -29,9 +29,13 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
   try{
    const [rows] = await db.execute(`
-     Select wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username`);
+     Select wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
+     FROM WalkRequests wr
+     JOIN Dogs d ON wr.dog_id = d.dog_id
+     JOIN Users u ON d.owner_id = u.user_id
+     WHERE wr.status = 'open'`);
      res.json(rows);
   } catch (err){
-     res.status(500).json({error: 'Failed to fetch dogs', details: err.message});
+     res.status(500).json({error: 'Failed to fetch  walk requests', details: err.message});
   }
  });
