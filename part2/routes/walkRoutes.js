8ce1d42a86996f1bb7ecfa48d1e943d9e7f80b84
perryx05 <minhchien0305@gmail.com>
2025-06-20 13:46:41 +0000

    [automatic commit] 2025-06-20 13:46:41 UTC

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -73,4 +73,17 @@ router.get('/my-dogs', async (req, res) => {
   }
 });
 
+app.get('/api/dogs', async (req, res) => {
+  try{
+   const [rows] = await db.execute(`
+     Select d.name AS dog_name, d.size, u.username AS owner_username
+     FROM Dogs d
+     JOIN Users u ON d.owner_id = u.user_id`);
+     res.json(rows);
+  } catch (err){
+     res.status(500).json({error: 'Failed to fetch dogs', details: err.message});
+  }
+ });
+
+
 module.exports = router;
