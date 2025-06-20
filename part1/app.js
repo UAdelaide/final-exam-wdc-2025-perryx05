e746be637403444610af349c9867214302830a00
perryx05 <minhchien0305@gmail.com>
2025-06-20 06:01:24 +0000

    [automatic commit] 2025-06-20 06:01:24 UTC

--- part1/app.js
+++ part1/app.js
@@ -17,6 +17,7 @@ async function initDB() {
 app.get('/api/dogs', async (req, res) => {
  try{
   const [rows] = await db.execute(`
-    Select d.name AS dog_name`)
+    Select d.name AS dog_name, d.size, u.username AS owner_username
+    FROM Dogs d`)
  }
 });
